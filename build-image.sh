4dd5d8ca6cfc084a77c59ad8ff706d312f91f554
Paul Knopf <pauldotknopf@gmail.com>
2019-11-24 23:11:16 -0500

    Created two rootfs, one for building the boot, and one for the runtime (actual booting).

--- build-image.sh
+++ build-image.sh
@@ -29,22 +29,18 @@ mkfs.ext4 -i 8192 -L data -U c83dfb09-d802-4de8-bf2c-b558552c4bd4 ${loop_device}
 # Mount the new partitions
 rm -rf rootfs-mounted && mkdir rootfs-mounted
 mount ${loop_device}p2 rootfs-mounted
-mkdir rootfs-mounted/boot
-mount ${loop_device}p1 rootfs-mounted/boot
 cp -a rootfs/* rootfs-mounted
 
-# Install GRUB
-arch-chroot rootfs-mounted grub-install ${DEVICE} --target=x86_64-efi --efi-directory=/boot
-arch-chroot rootfs-mounted grub-mkconfig -o /boot/grub/grub.cfg
-
 # Set the fstab
-echo "UUID=cf35024a-90c3-4ee7-b04a-7594f6ff48a0	/         	ext4      	rw,relatime,data=ordered	0 1" >> rootfs-mounted/etc/fstab
-echo "UUID=AB91-8E58      	/boot     	vfat      	rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,errors=remount-ro	0 2" >> rootfs-mounted/etc/fstab
+#echo "UUID=cf35024a-90c3-4ee7-b04a-7594f6ff48a0	/         	ext4      	rw,relatime,data=ordered	0 1" >> rootfs-mounted/etc/fstab
+#echo "UUID=AB91-8E58      	/boot     	vfat      	rw,relatime,fmask=0022,dmask=0022,codepage=437,iocharset=iso8859-1,shortname=mixed,errors=remount-ro	0 2" >> rootfs-mounted/etc/fstab
 
 # Clean up
-umount rootfs-mounted/boot
 umount rootfs-mounted
 rm -r rootfs-mounted
 losetup -d ${loop_device}
 
-qemu-img convert -O vmdk boot.img boot.vmdk
+#qemu-img convert -O vmdk boot.img boot.vmdk
+
+#kvm -drive format=raw,file=test.img -serial stdio -m 4G -cpu host -smp 2
+#kvm --bios /usr/share/qemu/OVMF.fd -net none -drive format=raw,file=test.img -serial stdio -m 4G -cpu host -smp 2
--- build-rootfs.sh
+++ build-rootfs.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-apt-tool install
-apt-tool generate-rootfs --overwrite --run-stage2
+bash -c "cd ./images/runtime && apt-tool install && apt-tool generate-rootfs --overwrite --run-stage2"
+bash -c "cd ./images/boot && apt-tool install && apt-tool generate-rootfs --overwrite --run-stage2"
